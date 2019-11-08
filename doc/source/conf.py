54380ddf97aaf3aaad709b9a693db033531814bc
Weizhe Yang <845161576@qq.com>
2019-11-09 03:55:21 +0800

    Release 0.4.1

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, RoniaWZ'
 author = 'RoniaWZ'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.0'
+release = '0.4.1'
 
 
 # -- General configuration ---------------------------------------------------
