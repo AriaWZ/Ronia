88193b1414e627ee7e038432de9db48207a13f60
Weizhe Yang <845161576@qq.com>
2020-05-08 23:30:38 +0800

    Release 0.4.5

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, RoniaWZ'
 author = 'RoniaWZ'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.4'
+release = '0.4.5'
 
 
 # -- General configuration ---------------------------------------------------
