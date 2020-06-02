fded99e47d4379d6cdc031cc0027127746c92c67
Weizhe Yang <845161576@qq.com>
2020-06-03 06:58:29 +0800

    Release 0.4.6

--- ronia/__version__.py
+++ ronia/__version__.py
@@ -10,7 +10,7 @@
 __title__ = "ronia"
 __description__ = "Generalized additive models with a Bayesian twist"
 __url__ = "https://roniawz.github.io/ronia"
-__version__ = "0.4.5"
+__version__ = "0.4.6"
 __author__ = "Weizhe Yang"
 __license__ = "MIT"
 __copyright__ = "Copyright 2020, Weizhe Yang"
